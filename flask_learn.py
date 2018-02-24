109467b2229a1d0a535b31dc9e2244fb2e50bced
lizhenguo <lizhenguo@xianjincard.com>
2018-02-24 14:05:33 +0800

    返回response对象

--- flask_learn.py
+++ flask_learn.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask import make_response
+
 app = Flask(__name__)
 
 
@@ -8,14 +10,22 @@ def index():
     return '<h1>Hello world</h1>'
 
 
+# 第二个参数200 表示返回的状态码：
 @app.route('/hello/<name>')
 def hello(name):
-    return '<h2> hello %s' % name, 200 # 第二个参数200 表示返回的状态码
+    return '<h2> hello %s' % name, 200
+
+
+# 返回response对象，并设置cookie：
+@app.route('/return/response')
+def return_reponse():
+    response = make_response("<h1>This document carries a cookie!</h1>")
+    response.set_cookie('answer', '42')
+    return response
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 # 执行命令:
 # export FLASK_APP=flask_learn.py
